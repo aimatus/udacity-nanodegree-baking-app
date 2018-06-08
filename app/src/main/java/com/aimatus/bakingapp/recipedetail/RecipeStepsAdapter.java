4d3bb0413f156b79f71c445a6fe090f4a938e5ea
Abraham Matus <aimatusco@gmail.com>
2018-06-08 13:02:17 -0500

    Updates recipe steps list styles.

--- app/src/main/java/com/aimatus/bakingapp/recipedetail/RecipeStepsAdapter.java
+++ app/src/main/java/com/aimatus/bakingapp/recipedetail/RecipeStepsAdapter.java
@@ -1,6 +1,7 @@
 package com.aimatus.bakingapp.recipedetail;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -18,6 +19,7 @@ public class RecipeStepsAdapter extends RecyclerView.Adapter<RecipeStepsAdapter.
 
     private static List<Step> steps;
     private static RecipeStepsFragment.OnStepClickListener onStepClickListener;
+    private View lastSelectedItemView;
 
     RecipeStepsAdapter(Recipe recipe, RecipeStepsFragment.OnStepClickListener onStepClickListener) {
         steps = recipe.getSteps();
@@ -52,22 +54,52 @@ public class RecipeStepsAdapter extends RecyclerView.Adapter<RecipeStepsAdapter.
         return steps.size();
     }
 
-    public static class RecipeStepsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public  class RecipeStepsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         final TextView stepTitleTextView;
         final TextView stepNumberTextView;
+        final View listDivider;
+        final boolean isLargeScreen;
 
         RecipeStepsViewHolder(View itemView) {
             super(itemView);
             stepTitleTextView = itemView.findViewById(R.id.tv_recipe_step);
             stepNumberTextView = itemView.findViewById(R.id.tv_recipe_step_number);
+            listDivider = itemView.findViewById(R.id.horizontal_separator);
             itemView.setOnClickListener(this);
+            isLargeScreen = itemView.getResources().getInteger(R.integer.grid_columns) > 1;
         }
 
         @Override
         public void onClick(View view) {
             int adapterPosition = getAdapterPosition();
             onStepClickListener.onStepSelected(adapterPosition);
+            if (isLargeScreen) {
+                applyLargeScreenStyles(view);
+            }
+        }
+
+        private void applyLargeScreenStyles(View view) {
+            applyColorAccent(view);
+            if (lastSelectedItemView != null) {
+                removeColorAccent(view);
+            }
+            lastSelectedItemView = view;
+        }
+
+        private void removeColorAccent(View view) {
+            int stepNumberBackgroundColor = view.getResources().getColor(R.color.stepNumberBackgroundColor);
+            int separatorColor = view.getResources().getColor(R.color.light_gray);
+            TextView lastSelectedStepNumberTextView = lastSelectedItemView.findViewById(R.id.tv_recipe_step_number);
+            View lastSelectedItemViewSeparator = lastSelectedItemView.findViewById(R.id.horizontal_separator);
+            lastSelectedStepNumberTextView.setBackgroundColor(stepNumberBackgroundColor);
+            lastSelectedItemViewSeparator.setBackgroundColor(separatorColor);
+        }
+
+        private void applyColorAccent(View view) {
+            int colorAccent = view.getResources().getColor(R.color.colorAccent);
+            stepNumberTextView.setBackgroundColor(colorAccent);
+            listDivider.setBackgroundColor(colorAccent);
         }
     }
 
